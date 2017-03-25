a4e6fb4ab2dff6710d6e21358a184da07df93ad4
Richard Sim <rdsim8589@gmail.com>
2017-03-24 22:02:07 -0700

    fixed the path

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix a type in the php
 exec { 'correct php typo':
   cwd     => "/var/www/html",
-  path    => "/usr/bin:/usr/sbin:/bin",
+  path    => "/usr/bin/:/usr/sbin/:/bin/",
   command => "sed -i 's/phpp/php/' wp-settings.php",
 }
