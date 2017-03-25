c4d4c9fd88ed4c34aff429deee0c0d83d6065e19
Richard Sim <rdsim8589@gmail.com>
2017-03-24 21:32:34 -0700

    removing the . with sed

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'correct php typo':
   cwd     => '/var/www/html',
   path    => '/usr/bin:/usr/sbin:bin',
-  command => 'sed -i "s/.phpp/php/" wp-settings.php',
+  command => 'sed -i "s/phpp/php/" wp-settings.php',
 }
