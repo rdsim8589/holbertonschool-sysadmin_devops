712104e2f28f914c326d05d5e8f0b1619f7228d8
Richard Sim <rdsim8589@gmail.com>
2017-03-24 22:03:49 -0700

    made lint happy

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # fix a type in the php
 exec { 'correct php typo':
-  cwd     => "/var/www/html",
-  path    => "/usr/bin/:/usr/sbin/:/bin/",
-  command => "sed -i 's/phpp/php/' wp-settings.php",
+  path    => '/usr/bin/:/usr/sbin/:/bin/',
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
 }
