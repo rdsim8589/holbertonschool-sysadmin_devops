1b36f976f841b8e18a8867242cc45f8a10031857
Richard Sim <rdsim8589@gmail.com>
2017-04-17 20:29:43 -0700

    completed export to csv"

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+This modules returns the to do list of an employee based on id
+
+This is done with eh CSV format
+"""
+import csv
+import requests
+import sys
+
+
+def get_user_info_csv(user_id):
+    url_root = "https://jsonplaceholder.typicode.com"
+    url_users = url_root + "/users"
+    url_todos = url_root + "/todos"
+    name = requests.get("{}/{}".format(url_users, user_id)).json().get("username")
+    r = requests.get("{}/?userId={}".format(url_todos, user_id)).json()
+    with open("{}.csv".format(user_id), "w", newline="") as f:
+        csvwriter = csv.writer(f, delimiter=",", quotechar='"',
+                              quoting=csv.QUOTE_ALL)
+        csvwriter.writerows([user_id, name, task.get("completed"),
+                            task.get("title")] for task in r)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        get_user_info_csv(sys.argv[1])
