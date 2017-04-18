72aed89d17d36269c1e66f859cd0d2bee43a211c
Richard Sim <rdsim8589@gmail.com>
2017-04-17 20:09:39 -0700

    completed getting employee todo

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -2,5 +2,22 @@
 """
 This modules returns the to do list of an employee based on id
 """
+import requests
+import sys
+
+
+def get_user_info(user_id):
+    url_root = "https://jsonplaceholder.typicode.com"
+    url_users = url_root + "/users"
+    url_todos = url_root + "/todos"
+    name = requests.get("{}/{}".format(url_users, user_id)).json().get("name")
+    r = requests.get("{}/?userId={}".format(url_todos, user_id)).json()
+    task_done = [task for task in r if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(task_done), len(r)))
+    for task in task_done:
+        print("\t{}".format(task.get("title")))
+
 if __name__ == "__main__":
-    
+    if len(sys.argv) == 2:
+        get_user_info(sys.argv[1])
