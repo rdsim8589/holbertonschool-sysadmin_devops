a3053973ee5222527b5fa663ab2435ae91ce98c5
Richard Sim <rdsim8589@gmail.com>
2017-05-25 10:08:09 -0700

    completed debugging assignment 0

---
+++ 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,31 @@
+#changes ulimit of nginx and restarts the service
+$str = '# Note: You may want to look at the following page before setting the ULIMIT.
+#  http://wiki.nginx.org/CoreModule
+#worker_rlimit_nofile
+# Set the ulimit variable if you need defaults to change.
+#  Example: ULIMIT="-n 4096"
+ULIMIT="-n 4096"'
+
+file { '/etc/default/nginx':
+  ensure  => file,
+  path    => '/etc/default/nginx',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => '$str',
+}
+
+service { 'nginx restart':
+  ensure     => 'running',
+  name       => 'nginx',
+  hasrestart => true,
+  path       => '/etc/init.d/',
+  restart    => true,
+}
+# display output of running the ab command
+#
+#exec { 'run ab':
+#  path      => '/usr/bin',
+#  command   => 'ab -c 100 -n 2000 localhost/',
+#  logoutput => true,
+#}
