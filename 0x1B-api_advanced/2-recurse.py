562dd783fa3161986611002a3336abbe134c49a8
Richard Sim <rdsim8589@gmail.com>
2017-05-26 10:18:20 -0700

    create recursive search to grab all hot post:

--- 0x1B-api_advanced/2-recurse.py
+++ 0x1B-api_advanced/2-recurse.py
@@ -4,30 +4,30 @@ Contains the recurse method
 """
 import random
 import requests
+user_agent = "User Agent{:d}".format(random.randrange(1000, 9999))
+header = {'User-Agent': user_agent}
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=""):
     """
     Creates a list of all the titles of the hot list
     input:
         subreddit: the name of a reddit subreddit
-        hot_list: list to be filled with the sub reddit name
+        hot_list: list to be filled with the subreddit hot post
     return: host_list or None otherwise
     """
-    user_agent = "User Agent{:d}".format(random.randrange(1000, 9999))
-    header = {'User-Agent': user_agent}
-    after = ""
-    while True:
-        url = "https://www.reddit.com/r/{}/.json{}".format(subreddit, after)
-        r = requests.get(url, headers=header, allow_redirects=False)
-        if r.status_code == 200:
-            r_dict = r.json()
-            for reddit_post in r_dict["data"]["children"]:
-                hot_list.append(reddit_post['data']['title'])
-            after = r_dict['data']['after']
-            if after is None:
-                break
-            after = "?after={}".format(after)
+    url = "https://www.reddit.com/r/{}/.json{}".format(subreddit, after)
+    r = requests.get(url, headers=header, allow_redirects=False)
+    if r.status_code == 200:
+        r_dict = r.json()
+        for reddit_post in r_dict["data"]["children"]:
+            hot_list.append(reddit_post['data']['title'])
+        after = r_dict['data']['after']
+        if after is None:
+            return hot_list
         else:
-            return None
+            after = "?after={}".format(after)
+            recurse(subreddit, hot_list, after)
+    else:
+        return None
     return hot_list
