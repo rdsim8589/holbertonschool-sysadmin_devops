d74596bc0ac0689cbca4accc794ce1cab29eb520
Richard Sim <rdsim8589@gmail.com>
2017-05-26 10:18:46 -0700

    created iterative version of grabbing all reddit top post

---
+++ 0x1B-api_advanced/2-recurse_interative.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Contains the recurse method
+"""
+import random
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Creates a list of all the titles of the hot list
+    input:
+        subreddit: the name of a reddit subreddit
+        hot_list: list to be filled with the sub reddit name
+    return: host_list or None otherwise
+    """
+    user_agent = "User Agent{:d}".format(random.randrange(1000, 9999))
+    header = {'User-Agent': user_agent}
+    after = ""
+    while True:
+        url = "https://www.reddit.com/r/{}/.json{}".format(subreddit, after)
+        r = requests.get(url, headers=header, allow_redirects=False)
+        if r.status_code == 200:
+            r_dict = r.json()
+            for reddit_post in r_dict["data"]["children"]:
+                hot_list.append(reddit_post['data']['title'])
+            after = r_dict['data']['after']
+            if after is None:
+                break
+            after = "?after={}".format(after)
+        else:
+            return None
+    return hot_list
