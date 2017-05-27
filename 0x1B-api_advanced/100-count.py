79bd8310b1944c670e37ef26ce1c4226c1ec3215
Richard Sim <rdsim8589@gmail.com>
2017-05-26 17:17:12 -0700

    trying to figure out checkeres

--- 0x1B-api_advanced/100-count.py
+++ 0x1B-api_advanced/100-count.py
@@ -2,14 +2,11 @@
 """
 Contains the recurse method
 """
-from collections import defaultdict
 import random
 import requests
-user_agent = "User Agent{:d}".format(random.randrange(1000, 9999))
-header = {'User-Agent': user_agent}
 
 
-def count_words(subreddit, word_list, base=0, keywords={}, after=""):
+def count_words(subreddit, word_list):
     """
     Creates a list of all the titles of the hot list
     input:
@@ -17,34 +14,34 @@ def count_words(subreddit, word_list, base=0, keywords={}, after=""):
         hot_list: list to be filled with the sub reddit name
     return: host_list or None otherwise
     """
-    if keywords == {}:
-        word_list = [word.lower() for word in word_list]
-        word_list = list(set(word_list))
-        keywords = defaultdict(int)
-    url = "https://www.reddit.com/r/{}/.json".format(subreddit)
-    r = requests.get(url, headers=header, params={"after": after},
-                     allow_redirects=False)
-    if r.status_code == 200:
-        r_dict = r.json()
-        for reddit_post in r_dict["data"]["children"]:
-            title_words = reddit_post['data']['title'].lower().split()
-            for title_word in title_words:
-                for word in word_list:
-                    if word == title_word:
-                        keywords[word] += 1
-        after = r_dict['data']['after']
-        if after is None:
-            return keywords
+    user_agent = "User Agent{:d}".format(random.randrange(1000, 9999))
+    header = {'User-Agent': user_agent}
+    after = ""
+    keywords = {}
+
+    for word in word_list:
+        keywords[word.lower()] = 0
+
+    while True:
+        url = "https://www.reddit.com/r/{}/.json{}".format(subreddit, after)
+        r = requests.get(url, headers=header, allow_redirects=False)
+        if r.status_code == 200:
+            r_dict = r.json()
+            for reddit_post in r_dict["data"]["children"]:
+                title_words = reddit_post['data']['title'].lower().split()
+                for title_word in title_words:
+                    for word in keywords.keys():
+                        if word == title_word:
+                            keywords[word] += 1
+            after = r_dict['data']['after']
+            if after is None:
+                break
+            after = "?after={}".format(after)
         else:
-            count_words(subreddit, word_list,
-                                   base + 1, keywords, after)
-    else:
-        return None
-    # only to be executed for before the recursive
-    if base is 0:
-        keywords_list = list(keywords.items())
-        keywords_list = sorted(keywords_list, key=lambda x: x[1], reverse=True)
-        for pair in keywords_list:
-            if pair[1] is not 0:
-                print("{}: {}".format(pair[0], pair[1]))
+            return None
+    keywords_list = list(keywords.items())
+    keywords_list = sorted(keywords_list, key=lambda x: x[1], reverse=True)
+    for pair in keywords_list:
+        if pair[1] is not 0:
+            print("{}: {}".format(pair[0], pair[1]))
     return keywords
